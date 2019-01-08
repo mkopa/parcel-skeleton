27fedd79634740254d959dfd019e8e8f2b5af8be
metapsj <patrickjohnson@forty3sixteen.com>
2019-01-08 15:09:07 -0800

    stub index, scss, and js

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+import {$, jQuery} from 'jquery';
+import Popper from 'popper.js';
+import Holder from 'holderjs';
+
+Holder.addTheme('thumb', {
+  bg: '#55595c',
+  fg: '#eceeef',
+  text: 'Thumbnail'
+});
+**/
+
